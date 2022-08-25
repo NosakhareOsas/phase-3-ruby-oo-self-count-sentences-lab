69acea291d2849340984f1253a3cb525386100b5
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-08-25 16:37:00 +0100

    test passed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split (/[\.\?\!]/)
+    clean_array = array.reject { |e| e == "" }
+    clean_array.size
+    
+    #self.match(/[\.?!]/)
   end
 end
+
+
+#binding.pry
